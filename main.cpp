80529a6c243b3aba3d6c30a1da7c812563e3c88d
machukhinktato <tarabrinmv@gmail.com>
2021-07-29 10:09:15 +0300

    toolbar added

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 //#include "simplemenu.h"
 //#include "anothermenu.h"
-#include "checkable.h"
-
+//#include "checkable.h"
+#include "toolbar.h"
 
 int main(int argc, char *argv[]){
     QApplication app(argc, argv);
 
 //    SimpleMenu window;
 //    AnotherMenu window;
-    Checkable window;
+//    Checkable window;
+    Toolbar window;
 
     window.resize(350,250);
 //    window.setWindowTitle("SimpleMenu");
 //    window.setWindowTitle("AnotherMenu");
-    window.setWindowTitle("Checkable menu");
+    window.setWindowTitle("QToolBar");
     window.show();
 
     return app.exec();
